c2c0b261a5a93fc3e98afa323fbf4e7dbf9a1ba2
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-12-03 19:23:21 +0700

    refactor: replace logrus with zerolog and improve logging messages

--- cmd/main.go
+++ cmd/main.go
@@ -2,32 +2,31 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
+	"github.com/rs/zerolog/log"
 
 	"github.com/sgitwhyd/music-catalogue/internal/configs"
+	"github.com/sgitwhyd/music-catalogue/internal/models"
 	"github.com/sgitwhyd/music-catalogue/pkg/internalsql"
 )
 
 func main() {
 	config, err := configs.Init("./", "env", ".env")
 	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err.Error(),
-		}).Info("failed load config")
+		log.Error().Err(err).Msgf("load config error %v", err)
 	}
-	log.Infoln("load config success")
+	log.Info().Msg("load config success")
 
-	_, err = internalsql.Connect(config.DatabaseURL)
+	db, err := internalsql.Connect(config.DatabaseURL)
 	if err != nil {
-	log.WithFields(log.Fields{
-			"error": err.Error(),
-		}).Info("failed connecting database")
+		log.Error().Err(err).Msgf("error db connection %v", err)
 	}
-	log.Info("database connected")
+	log.Info().Msg("database connected")
 
 	if config.ENV == "release" {
 		gin.SetMode(gin.ReleaseMode)
 	}
+
+	db.AutoMigrate(&models.User{})
 
 	route := gin.Default()
 	route.Run(config.PORT)
