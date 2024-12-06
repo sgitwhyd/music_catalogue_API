f1ba7a041d5f3364b02d85b89a5b87f87f291bcf
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-12-06 15:22:12 +0700

    test: add unit tests for user sign-in functionality and mock service methods

--- internal/handlers/user_handler_test.go
+++ internal/handlers/user_handler_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -62,14 +63,14 @@ func Test_userHandler_SignUp(t *testing.T) {
 				mockSvc.EXPECT().Register(
 					models.SignUpRequest{
 						Username: "developer",
-						Email: "developer@testing.com",
+						Email:    "developer@testing.com",
 						Password: "password",
 					},
 				).Return(errors.New("username or email already registered"))
 			},
 			requestBody: models.SignUpRequest{
 				Username: "developer",
-				Email:   "developer@testing.com",
+				Email:    "developer@testing.com",
 				Password: "password",
 			},
 			expectedStatusCode: 400,
@@ -84,7 +85,7 @@ func Test_userHandler_SignUp(t *testing.T) {
 			route := r.Group("/api/v1")
 
 			h := &userHandler{
-				route:      route,
+				route:       route,
 				userService: mockSvc,
 			}
 
@@ -103,6 +104,127 @@ func Test_userHandler_SignUp(t *testing.T) {
 			r.ServeHTTP(w, httpReq)
 
 			assert.Equal(t, tt.expectedStatusCode, w.Code)
+		})
+	}
+}
+
+func Test_userHandler_SignIn(t *testing.T) {
+	ctrlMock := gomock.NewController(t)
+
+	mockSvc := NewMockUserService(ctrlMock)
+
+
+	tests := []struct {
+		name string
+		requestBody        	models.SignInRequest
+		expectedStatusCode 	int
+		expectedBody				models.LoginResponse
+		mockFn 							func ()
+		wantErr							bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "should error when required request body not filled",
+			requestBody: models.SignInRequest{
+				Email: "",
+				Password: "password",
+			},
+			expectedStatusCode: 422,
+			wantErr: true,
+			expectedBody: models.LoginResponse{},
+			mockFn: func() {
+				mockSvc.EXPECT().Login(gomock.Any()).Times(0)
+			},
+		},
+		{
+			name: "should error when email not registered",
+			requestBody: models.SignInRequest{
+				Email: "developer@gmail.com",
+				Password: "password",
+			},
+			expectedStatusCode: 500,
+			wantErr: true,
+			expectedBody: models.LoginResponse{},
+			mockFn: func() {
+				mockSvc.EXPECT().Login(models.SignInRequest{
+					Email: "developer@gmail.com",
+					Password: "password",
+				}).Return("", fmt.Errorf("email not registered"))
+			},
+		},
+		{	
+			name: "should error when password doesn't match",
+			requestBody: models.SignInRequest{
+				Email: "developer@gmail.com",
+				Password: "password",
+			},
+			expectedStatusCode: 500,
+			wantErr: true,
+			expectedBody: models.LoginResponse{},
+			mockFn: func() {
+				mockSvc.EXPECT().Login(models.SignInRequest{
+					Email: "developer@gmail.com",
+					Password: "password",
+				}).Return("", fmt.Errorf("password doesn't match"))
+			},
+		},
+		{	
+			name: "should success when email and password match and registered",
+			requestBody: models.SignInRequest{
+				Email: "developer@gmail.com",
+				Password: "password",
+			},
+			expectedStatusCode: 200,
+			expectedBody: models.LoginResponse{
+				AccessToken: "valid token",
+			},
+			wantErr: false,
+			mockFn: func() {
+				mockSvc.EXPECT().Login(models.SignInRequest{
+					Email: "developer@gmail.com",
+					Password: "password",
+				}).Return("valid token", nil)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mockFn()
+
+			r := gin.New()
+			route := r.Group("/api/v1")
+
+			h := &userHandler{
+				route:       route,
+				userService: mockSvc,
+			}
+
+			h.RegisterRoute()
+
+			w := httptest.NewRecorder()
+
+			endpoint := "/api/v1/auth/signin"
+			bodyBytes, err := json.Marshal(tt.requestBody)
+			assert.NoError(t, err)
+
+			body := bytes.NewReader(bodyBytes)
+			httpReq, err := http.NewRequest(http.MethodPost, endpoint, body)
+			assert.NoError(t, err)
+
+			r.ServeHTTP(w, httpReq)
+
+			assert.Equal(t, tt.expectedStatusCode, w.Code)
+
+			if !tt.wantErr {
+				res := w.Result()
+				defer res.Body.Close()
+
+				response := models.LoginResponse{}
+				err := json.Unmarshal(w.Body.Bytes(), &response)
+				assert.NoError(t, err)
+
+				assert.Equal(t, tt.expectedBody, response)
+			}
 		})
 	}
 }
